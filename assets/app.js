ae1b8eb842024b4b51c6262b31003bdd3f05396e
Matt McFadyen <mmcfadyen91@gmail.com>
2014-11-09 21:49:54 -0500

    services in :ng:

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,23 @@
 var app = angular.module('app',[]);
 
-app.controller('PostsCtrl', function($scope, $http){
+app.service("PostsSvc", function ($http) {
+	
+	this.fetch = function() {
+		return $http.get('/api/posts');
+	}
+
+	this.create = function(post) {
+		return $http.post('/api/posts', post);
+	}
+});
+
+app.controller('PostsCtrl', function($scope, PostsSvc){
 	
 	$scope.addPost = function(){
 
 		if ($scope.postBody) {
 			
-			$http.post('/api/posts', {
+			PostsSvc.create({
 				username: 'devloper plz work',
 				body: $scope.postBody 
 			}).success(function (post) {
@@ -18,9 +29,8 @@ app.controller('PostsCtrl', function($scope, $http){
 		}
 	};
 
-	$http.get('/api/posts')
-		 .success(function (posts) {
-		 	$scope.posts = posts;
+	PostsSvc.fetch().success(function (posts){
+		$scope.posts = posts;
 	});
 
 });
