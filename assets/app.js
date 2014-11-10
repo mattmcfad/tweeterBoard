9ffb16f20812cee0b996f4b5fc13899005378e48
Matt McFadyen <mmcfadyen91@gmail.com>
2014-11-09 21:39:43 -0500

    tidying up code :pizza::cat:

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('app',[]);
+
+app.controller('PostsCtrl', function($scope, $http){
+	
+	$scope.addPost = function(){
+
+		if ($scope.postBody) {
+			
+			$http.post('/api/posts', {
+				username: 'devloper plz work',
+				body: $scope.postBody 
+			}).success(function (post) {
+				$scope.posts.unshift(post);
+				$scope.postBody = null;
+			}).error(function(err){
+				console.log(err);
+			})
+		}
+	};
+
+	$http.get('/api/posts')
+		 .success(function (posts) {
+		 	$scope.posts = posts;
+	});
+
+});
--- server.js
+++ server.js
@@ -1,43 +1,14 @@
 var express = require('express'),
-	bodyParser = require('body-parser'),
-	Post = require('./models/post');
+	bodyParser = require('body-parser');
 
 var	app = express(),
 	port = 8888;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-	// res.status(200).send('<h1>hello world </h1>');
-	res.sendfile('layouts/posts.html');
-});
 
-
-
-app.get('/api/posts', function (req, res, next) {
-	Post.find(function(err, posts) {
-		if (err) {
-			return next(err)
-		}
-		res.json(posts);
-	})	
-});
-
-app.post('/api/posts', function(req, res, next) {
-	
-	var post = new Post({
-		username: req.body.username,
-		body: req.body.body
-	});
-	
-	post.save(function(err, post){
-		if (err) {
-			console.log("fac");
-			return next(err);
-		}
-		res.status(201).json(post);
-	})
-});
+app.use(require('./controllers/api/posts'));
+app.use(require('./controllers/static'));
 
 app.listen(port, function(){
 	console.log("listening on localhost:" + port);
