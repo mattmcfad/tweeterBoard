91192d62ef49b04d9b28d5dfe79fe8fd886c81cc
Matt McFadyen <mmcfadyen91@gmail.com>
2014-12-26 18:18:52 -0500

    server rendering jade views

--- controllers/static.js
+++ controllers/static.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
-router.get('/', function (req, res) {
-	res.sendfile('layouts/posts.html');
+// router.get('/', function (req, res) {
+// 	res.sendfile('layouts/posts.html');
+// });
+
+router.get('/', function(req, res) {
+	res.render("index.jade", {dev: {name: "matt"}});
 });
 
 // make filepath assets
-router.use(express.static(__dirname + "/../assets"));
+//router.use(express.static(__dirname + "/public/assets/"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,16 @@ var	app = express(),
 	port = 8888;
 
 app.use(bodyParser.json());
+app.engine('jade', require('jade').__express);
+app.set('views', __dirname + '/public/assets/views')
 
 
 app.use(require('./controllers/api/posts'));
 app.use(require('./controllers/static'));
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/assets/"));
+
 
 app.listen(port, function(){
 	console.log("listening on localhost:" + port);
