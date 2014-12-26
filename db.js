7cacb5e1bc48da7e416a0c5fa5f5621832730604
Matt McFadyen <mmcfadyen91@gmail.com>
2014-12-26 18:16:59 -0500

    switched db to mongolabs

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/social', function () {
+mongoose.connect('mongodb://dev:walmartdev@ds047020.mongolab.com:47020/nodetweeter', function () {
 	console.log('mongodb connected');
 });
 
