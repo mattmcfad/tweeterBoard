c52e8bcd1b6cf73b17fc49293e8b40a768af8e3e
Matt McFadyen <mmcfadyen91@gmail.com>
2014-12-26 18:32:17 -0500

    fixed up jade

--- controllers/static.js
+++ controllers/static.js
@@ -1,15 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-// router.get('/', function (req, res) {
-// 	res.sendfile('layouts/posts.html');
-// });
-
 router.get('/', function(req, res) {
 	res.render("index.jade", {dev: {name: "matt"}});
 });
-
-// make filepath assets
-//router.use(express.static(__dirname + "/public/assets/"));
 
 module.exports = router;
