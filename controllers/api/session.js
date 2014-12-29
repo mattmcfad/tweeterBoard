04a642adc2bbb8261191213882858d3f72c77d86
Matt McFadyen <mmcfadyen91@gmail.com>
2014-12-29 18:43:08 -0500

    added secret key

--- controllers/api/session.js
+++ controllers/api/session.js
@@ -1,8 +1,11 @@
 var User = require ('../../models/user');
 var router = require('express').Router();
 var bcrypt = require('bcrypt');
+var jwt = require('jwt-simple');
+var secretKey = process.env.secretKey || require('../../key.coffee');
 
 router.post('/', function (req, res, next){
+	console.log("POST: Session", req.body.username, req.body.password);
 	User.findOne({username: req.body.username})
 		.select('password')
 		.exec(function(err, user) {
@@ -10,6 +13,7 @@ router.post('/', function (req, res, next){
 				return next(err);
 			}
 			if (!user) {
+				console.log("error: not a user");
 				return res.send(401);
 			}
 			bcrypt.compare(req.body.password, user.password, function(err, valid){
@@ -17,8 +21,10 @@ router.post('/', function (req, res, next){
 					return next(err);
 				}
 				if (!valid) {
+					console.log("error: pw not valid");
 					return res.send(401);
 				}
+
 				var token = jwt.encode({username: user.username}, secretKey);
 				res.json(token);
 			});
--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,25 +1,30 @@
 var User = require ('../../models/user');
 var router = require('express').Router();
 var bcrypt = require('bcrypt');
+var jwt = require('jwt-simple');
+var secretKey = process.env.secretKey || require('../../key.coffee');
 
 router.get('/', function(req, res, next) {
+	console.log("GET: users");
 	var token = req.headers['x-auth'];
 	var auth = jwt.decode(token, secretKey);
 	User.findOne({username: auth.username}, function(err, user){
+		console.log("GET: users results:", user);
 		res.json(user);
 	});
 });
 
 
 router.post('/', function(req, res, next) {
+	console.log("POST: users", req.body.username, req.body.password);
 	var user = new User({username: req.body.username});
 	bcrypt.hash(req.body.password, 10, function(err, hash) {
 		user.password = hash;
 		user.save(function(err, user) {
 			if (err) {
-				throw next(err);
+				console.error("error bruh!!!");
 			}
-			res.send(201);
+			res.sendStatus(201);
 		});
 	});
 });
